8dcda9d09477f067517b5f2b29b03f159ba3bdb6
Fabrizio Torres Daniele <fatorres@loteriadecordoba.com.ar>
2025-03-25 10:43:27 -0300

    agregando un findByName

--- my-backend-nuevo/src/trabajador/trabajador.controller.ts
+++ my-backend-nuevo/src/trabajador/trabajador.controller.ts
@@ -1,13 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { TrabajadorService } from './trabajador.service'; 
 import { Trabajador } from 'src/entities/trabajador.entity';
 
 @Controller('/api/trabajadores')
 export class TrabajadorController {
-  constructor(private readonly trabajadorService: TrabajadorService) {}
+  constructor(private trabajadorService: TrabajadorService) {}
 
   @Get()
   async findAll(): Promise<Trabajador[]> {
     return this.trabajadorService.findAll(); // Llama al servicio que obtiene todos los trabajadores
   }
+
+
+  @Get(':nombre')
+  async findById(@Param('nombre') nombre: string): Promise<Trabajador | null> {
+      return this.trabajadorService.findByName(nombre);
+  }
+  
+
 }
--- my-backend-nuevo/src/trabajador/trabajador.service.ts
+++ my-backend-nuevo/src/trabajador/trabajador.service.ts
@@ -13,5 +13,23 @@ export class TrabajadorService {
     async findAll(): Promise<Trabajador[]> {
         return this.trabajadorRepository.find(); // Usa el repositorio para obtener los datos
       }
+
+
+      async findByName(name: string): Promise<Trabajador | null> {
+        try {
+            const trabajador = await this.trabajadorRepository.findOne({ where: { name } });
+            
+            if (!trabajador) {
+                console.log('No hay trabajadores con ese nombre');
+                return null; // Explicitly return null if no trabajador is found
+            }
+    
+            return trabajador; // Return the trabajador if found
+        } catch (error) {
+            console.error('Error al buscar trabajador:', error);
+            throw new Error('Error al buscar trabajador'); // Throw the error to handle it properly elsewhere
+        }
+    }
+    
 }
 
