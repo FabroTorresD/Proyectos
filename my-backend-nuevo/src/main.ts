f2798e3376165a06986e2212293e5fe865ebb633
Fabrizio Torres Daniele <fatorres@loteriadecordoba.com.ar>
2025-03-25 11:06:45 -0300

    agregando mas cosas

--- my-backend-nuevo/src/main.ts
+++ my-backend-nuevo/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express'; // Importar el tipo NestExpressApplication
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  // Crear el servidor de Nest usando Express
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // Escuchar en el puerto (si existe, usa el valor de `PORT`, si no, usa 3000)
   await app.listen(process.env.PORT ?? 3000);
+  console.log(`Servidor corriendo en http://localhost:${process.env.PORT ?? 3000}`);
 }
 bootstrap();
